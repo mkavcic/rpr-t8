f8eeba695bf1ea91343f3d36614680cffd3a2010
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-10 20:40:40 +0100

    Napravljen izgled GUI-a, povezane komande, napravljena klasa Pretraga

--- src/ba/unsa/etf/rpr/tutorijal08/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Controller.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.tutorijal08;
 
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+    public TextField unos;
+
+    public void trazi(ActionEvent actionEvent){
+        String string=unos.getText();
+        System.out.println(string);
+    }
 }
