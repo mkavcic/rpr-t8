7a1c9338baebbbd331aa307c80790c483e1cf168
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-10 22:52:30 +0100

    Implementirana funkcija trazi (ne ispisuje u listu)

--- src/ba/unsa/etf/rpr/tutorijal08/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Controller.java
@@ -4,12 +4,28 @@ package ba.unsa.etf.rpr.tutorijal08;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
+import java.io.File;
+
 
 public class Controller {
     public TextField unos;
 
     public void trazi(ActionEvent actionEvent){
-        String string=unos.getText();
-        System.out.println(string);
+        walk("C:\\");
+    }
+
+    public void walk(String path){
+        File root = new File(path);
+        File[] list=root.listFiles();
+        if(list==null) return;
+        for(File f:list){
+            if(f.isDirectory()){
+                walk(f.getAbsolutePath());
+            }
+            else{
+                String s=f.getAbsoluteFile().toString();
+                if(s.contains(unos.getText())) System.out.println(s);
+            }
+        }
     }
 }
