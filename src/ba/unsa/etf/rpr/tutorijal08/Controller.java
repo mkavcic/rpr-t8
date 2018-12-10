98762a95ce96aaf4f2d707ee785d9dd85189d039
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-11 00:41:38 +0100

    napravljen Thread

--- src/ba/unsa/etf/rpr/tutorijal08/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal08/Controller.java
@@ -3,31 +3,47 @@ package ba.unsa.etf.rpr.tutorijal08;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.io.File;
+import java.util.concurrent.ExecutorService;
 
 
-public class Controller {
+public class Controller{
     public TextField unos;
     public ListView<String> list = new ListView<String>();
     ObservableList<String> items =FXCollections.observableArrayList ();
 
+    Task<ObservableList<String>> task=new Task<ObservableList<String>>(){
+        @Override protected ObservableList<String> call(){
+            walk("C:\\Users\\Mirna\\");
+            return items;
+        }
+    };
+
 
     public void trazi(ActionEvent actionEvent){
 
-        walk("C:\\Users\\Mirna\\Desktop\\");
+       // ExecutorService.submit(task);
+
+       // walk("C:\\Users\\Mirna\\Desktop\\");
+        Thread th = new Thread(task);
+
+        th.setDaemon(true);
+
+        th.start();
 
         list.setItems(items);
     }
 
     public void walk(String path){
         File root = new File(path);
-        File[] list=root.listFiles();
+        File[] lista=root.listFiles();
         if(list==null) return;
-        for(File f:list){
+        for(File f:lista){
             if(f.isDirectory()){
                 walk(f.getAbsolutePath());
             }
@@ -35,11 +51,12 @@ public class Controller {
                 String s=f.getAbsoluteFile().toString();
                 if(s.contains(unos.getText())){
                    items.add(s);
-
-                   System.out.println(s);
+                    list.setItems(items);
+                  // System.out.println(s);
                 }
             }
         }
 
     }
+
 }
